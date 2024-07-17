529ddef82f8f83db0fa915bac5c33008fbf8c612
Simon Hetzer <simon.hetzer@icloud.com>
2024-07-17 13:06:04 +0900

    Calibration Noise Reduction Test
    Dilate + erode seems to work good, test with proper calibration
    Then apply to c2plist in calibration step

--- main.cpp
+++ main.cpp
@@ -4,6 +4,23 @@ int main()
 {
     if (!ProjectorConfig::initGLFW()) return -1;
 
+    auto calibrationResult = imread("captured1/result.png");
+    Mat eroded, dilated;
+    int morphSize = 1;
+    auto kernelSize1 = getStructuringElement(MORPH_RECT, Size(2 * morphSize + 1, 2 * morphSize + 1), Point(morphSize, morphSize));
+    morphSize = 2;
+    auto kernelSize2 = getStructuringElement(MORPH_RECT, Size(2 * morphSize + 1, 2 * morphSize + 1), Point(morphSize, morphSize));
+    morphSize = 3;
+    auto kernelSize3 = getStructuringElement(MORPH_RECT, Size(2 * morphSize + 1, 2 * morphSize + 1), Point(morphSize, morphSize));
+
+    cv::dilate(calibrationResult, dilated, kernelSize3);
+    cv::erode(dilated, eroded, kernelSize2);
+
+    imwrite("eroded.png", eroded);
+    imwrite("dilated.png", dilated);
+    return 0;
+
+
     // Define the projector configuration
     ProjectorConfig projector1 = ProjectorConfig(1);
     ProjectorConfig projector2 = ProjectorConfig(2, &projector1);
