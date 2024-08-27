9f3d4513d1cc510be9f785b25c32a6c81da80b67
Simon Hetzer <simon.hetzer@icloud.com>
2024-08-27 14:53:33 +0900

    Final Project State
    - 3 configurations, one not correctly computing a homography
    - Uploaded captures and results

--- main.cpp
+++ main.cpp
@@ -7,22 +7,29 @@ int main()
     ProjectorConfig::CAMHEIGHT = 1080;
     ProjectorConfig::CAMWIDTH = 1920;
 
-    ProjectorConfig::initCamera();
+    // Only needed for calibration
+    //ProjectorConfig::initCamera();
 
-    const int PROJECTORCOUNT = 2;
+    const int PROJECTORCOUNT = 3;
     ProjectorConfig* projectors = new ProjectorConfig[PROJECTORCOUNT];
 
     // Define the projector configuration
     projectors[0] = ProjectorConfig(1);
     projectors[1] = ProjectorConfig(2, projectors);
-    //projectors[2] = ProjectorConfig(3, projectors); // Homography not found
+    projectors[2] = ProjectorConfig(3, projectors); // Homography not found
 
     // Calibrate/load projector configurations
     for (int i = 0; i < PROJECTORCOUNT; i++) {
 
+        // -------------- LOAD CONFIGURATION -----------------------
+        // ---- comment out if you want to calibrate instead -------
+
         // Load an existing configuration from folder "captured<i>"
-        //projectors[i].loadConfiguration();
-        //continue;
+        projectors[i].loadConfiguration();
+        // Apply optional area masking to help with overexposed projector 3
+        if (i == 2)
+            projectors[i].applyAreaMask();
+        continue;
 
         // -------------- CALIBRATE NEW CONFIGURATION ----------------
 
